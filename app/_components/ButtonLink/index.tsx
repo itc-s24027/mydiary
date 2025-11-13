312544edca2510beae1dc59366b11d46446ba5d3
itc-s24027 <s24027@std.it-college.ac.jp>
2025-11-13 21:40:06 +0900

    ボタンのコンポーネント作成

--- app/_components/ButtonLink/index.tsx
+++ app/_components/ButtonLink/index.tsx
@@ -1,3 +1,4 @@
+import styles from './index.module.css';
 
 type Props = {
     href: string;
@@ -6,7 +7,7 @@ type Props = {
 
 export default function ButtonLink({ href, children }: Props) {
     return (
-        <a href={href}>
+        <a href={href} className={styles.button}>
             {children}
         </a>
     );
