26808c9a0f6e4644786a73796f697e9e94f14681
itc-s24027 <s24027@std.it-college.ac.jp>
2025-11-13 21:46:06 +0900

    Headerを作成

--- app/_components/Header/index.tsx
+++ app/_components/Header/index.tsx
@@ -1,14 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "./index.module.css";
 
 export default function Header() {
-    return (
-      <header>
-        <h1>my diary</h1>
-        <nav>
-          <ul>
-            <li>Home</li>
-            <li>past diary</li>
-          </ul>
-        </nav>
-      </header>
-    );
+  return (
+    <header className={styles.header}>
+      <a href="/" className={styles.logoLink}>
+        <Image
+          src="/images/myDiarylogo.gif"
+          alt="Diary Logo"
+          className={styles.logo}
+          width={125}
+          height={95}
+          priority
+          unoptimized
+        />
+      </a>
+      <nav className={styles.headernav}>
+        <ul className={styles.navList}>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/diary">Diarys</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
